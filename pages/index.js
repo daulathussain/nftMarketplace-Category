92f4518e9ceecce902afe95a541529f53e1217b6
daulathussain <daulathussain92@gmail.com>
2022-08-27 10:41:23 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+//INTERNAL IMPORT
+import Style from "../styles/index.module.css";
+import {
+  HeroSection,
+  Service,
+  BigNFTSilder,
+  Subscribe,
+  Title,
+  Category,
+} from "../components/componentsindex";
+
+const Home = () => {
+  return (
+    <div className={Style.homePage}>
+      <HeroSection />
+      <Service />
+      <BigNFTSilder />
+      <Title
+        heading="Browse by category"
+        paragraph="Explore the NFTs in the most featured categories."
+      />
+      <Category />
+      <Subscribe />
+    </div>
+  );
+};
+
+export default Home;
